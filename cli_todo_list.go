fbfaf03d346a55141b7e5aa564171d9e82f47cf6
VojtechRichter <vojta3218@gmail.com>
2024-07-09 10:45:36 +0200

    functionality for updating task status

--- cli_todo_list.go
+++ cli_todo_list.go
@@ -3,6 +3,7 @@ package main
 import (
     "fmt"
     _ "os"
+    "slices"
 )
 
 const (
@@ -44,6 +45,40 @@ func (tl *TaskList) Display() {
         t.Display()
     }
     fmt.Printf("\n")
+}
+
+func (tl *TaskList) GetTaskById(id string) (Task, bool) {
+    for _, v := range *tl {
+        if v.ID == id {
+            return v, true
+        }
+    }
+
+    return Task{}, false
+}
+
+func (tl *TaskList) UpdateTaskCompletion(id string, complete bool) bool {
+    for i, _ := range *tl {
+        if (*tl)[i].ID == id {
+            (*tl)[i].Completed = complete
+
+            return true
+        }
+    }
+
+    return false
+}
+
+func (tl *TaskList) RemoveTask(id string) bool {
+    for i, _ := range *tl {
+        if (*tl)[i].ID == id {
+            // find a better way
+            *tl = slices.Delete(*tl, i, i+1)
+            return true
+        }
+    }
+
+    return false
 }
 
 func main() {
